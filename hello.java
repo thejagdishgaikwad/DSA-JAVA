da552ecd8ab610e7b66d3be9fc6ced23816f200e
Jagdish <jagdishgaikwad8765@gmail.com>
2025-04-16 20:07:19 +0530

    Initial Practice Files For DSA

---
+++ hello.java
@@ -0,0 +1,6 @@
+package CODE.Java;
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Jagdish!");
+    }
+}
---
+++ patterns.java
@@ -0,0 +1,8 @@
+public class patterns {
+    public static void main(String[]args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
